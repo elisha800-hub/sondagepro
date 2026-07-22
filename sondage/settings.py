6c5ef50226e6fb7f49b0d71a1d95b23fde07e671
elisha800 <elishaglodie56@gmail.com>
2026-07-22 10:30:35 +0200

    Fix CSRF trusted origins and survey creation form

--- sondage/settings.py
+++ sondage/settings.py
@@ -36,6 +36,8 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost:8000,http://127.0.0.1:8000,http://127.0.0.1:8417').split(',')
+
 
 # Application definition
 
